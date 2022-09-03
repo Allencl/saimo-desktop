adf398c379e4097be59dd4e1d4e1add06357bcd9
Allencl <Allen__cc@163.com>
2022-09-03 21:12:05 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Homefff</router-link> |
+    <router-link to="/about">Aboutddd</router-link>
   </nav>
   <router-view/>
 </template>
